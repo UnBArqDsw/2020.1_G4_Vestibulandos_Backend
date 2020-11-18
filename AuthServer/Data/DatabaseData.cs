6daabad14002df4ebc5331ec93e0e2f9d18c8a31
Julio Litwin <j.litwin@live.com>
2020-11-17 22:09:06 -0300

    Adicionado struct para DB.

---
+++ AuthServer/Data/DatabaseData.cs
@@ -0,0 +1,13 @@
+﻿namespace LoginServer.Data
+{
+    //---------------------------------------------------------------------------------------------------
+    public struct VerifyAccountData
+    {
+        public string Name;
+        public ulong UserID;
+        public byte[] SessionKey;
+        public string Ip;
+        
+        public int LastServerID;
+    }
+}
